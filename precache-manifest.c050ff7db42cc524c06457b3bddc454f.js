a0a84dd7236d6850c179e013a60dd0218e9c0657
Ihor-Onyshchuk <onyshchukihor11@gmail.com>
2020-05-18 21:30:08 +0300

    Updates

---
+++ precache-manifest.c050ff7db42cc524c06457b3bddc454f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8be6c5554c710d1eda7f8cb881f5c3d7",
+    "url": "/chuck-jokes/index.html"
+  },
+  {
+    "revision": "7ea58ea5091d963322f0",
+    "url": "/chuck-jokes/static/css/main.e674e16b.chunk.css"
+  },
+  {
+    "revision": "430ee712491a03323648",
+    "url": "/chuck-jokes/static/js/2.a10281f4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/chuck-jokes/static/js/2.a10281f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ea58ea5091d963322f0",
+    "url": "/chuck-jokes/static/js/main.6333fadc.chunk.js"
+  },
+  {
+    "revision": "1980b2a2e9fae2db5c6e",
+    "url": "/chuck-jokes/static/js/runtime-main.63892202.js"
+  }
+]);
